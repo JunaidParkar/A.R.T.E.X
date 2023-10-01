0322e4cd8190d664d8bbfa698aa7b87f90e45c46
JunaidParkar <parkarjunaid2811@gmail.com>
2023-10-02 02:42:21 +0530

    Started creating CMD engine

---
+++ Evo/Body/Comp/Js/cmd.js
@@ -0,0 +1,90 @@
+var commandHistory = [];
+var commandIndex = -1;
+
+function checkInput() {
+    var event = window.event || event.which;
+    if (event.keyCode == 13) {
+        var command = document.getElementById("textinput").value;
+        addLine(command, "self");
+        commandHistory.push(command);
+        commandIndex = commandHistory.length;
+        cmdExe(command)
+        document.getElementById("textinput").value = "";
+        document.getElementById("textinput").style.height = "1em";
+        event.preventDefault();
+    } else if (event.keyCode == 38) {
+        if (commandIndex > 0) {
+            commandIndex--;
+            document.getElementById("textinput").value = commandHistory[commandIndex];
+        }
+        event.preventDefault();
+    } else if (event.keyCode == 40) {
+        if (commandIndex < commandHistory.length - 1) {
+            commandIndex++;
+            document.getElementById("textinput").value = commandHistory[commandIndex];
+        }
+        event.preventDefault();
+    } else {
+        document.getElementById("textinput").style.height = "";
+        document.getElementById("textinput").style.height = document.getElementById("textinput").scrollHeight + "px";
+    }
+}
+
+function addLine(line, author = null, error = false) {
+    var d = document.createElement("div"),
+        p = document.createElement("p")
+    if (error) {
+        d.classList.add("error")
+    }
+    if (author == "self") {
+        let p2 = document.createElement("p")
+        p2.textContent = "Evolution AI >> "
+        d.appendChild(p2)
+    }
+    p.innerHTML = line;
+    d.appendChild(p)
+    document.getElementById("consoletext").appendChild(d);
+}
+
+const cmdExe = (c) => {
+    let mc = ["Evo"]
+    let imc = c.split(" ")
+    if (mc.includes(imc[0])) {
+        cmdEngine(c)
+    } else {
+        addLine(line = `${imc[0]} : The term '${imc[0]}' is not recognized as the name of a cmdlet, function, script file, or operable program. Check
+        the spelling of the name, or if a path was included, verify that the path is correct and try again.<br><br>
+        At line:1 char:1<br><br>
+        + ${imc[0]}`, null, true)
+    }
+}
+
+const cmdEngine = (command) => {
+    let c = command.split(" ")
+    let cf = []
+    let co = {
+        "evo": {
+            "flags": ["--h"]
+        }
+    }
+    for (let word of c) {
+        if (co[c[0].toLowerCase()].flags.includes(word)) {
+            cf.push(word)
+        }
+    }
+
+    if (c[0].toLowerCase() == "evo") {
+        let fi
+        for (const f of cf) {
+            if (f == "--h") {
+                fi = c.indexOf("--h")
+                el = c[fi - 1].toLowerCase()
+                if (el == "evo") {
+                    console.log("last")
+                    let d = `<table><tr><th width="100">Flags</th><th width="200">Usage</th></tr><tr><th>--h</th><td>Used to get help with all functionalities of Evolution AI CMD.</td></tr></table>`
+                    addLine(d)
+                }
+            }
+        }
+    }
+}
--- Evo/Body/Comp/Js/script.js
+++ Evo/Body/Comp/Js/script.js
@@ -1,66 +1,114 @@
-// function breakText(text, maxWidth) {
-//     var words = text.split(" ");
-//     var newText = "";
-//     var currentLine = "";
-//     for (var i = 0; i < words.length; i++) {
-//         if (currentLine.length + words[i].length + 1 > maxWidth) { // Add 1 for the hyphen
-//             newText += currentLine + "\n-";
-//             currentLine = words[i];
-//         } else if (currentLine.length + words[i].length >= maxWidth) { // If the current line is already at the max width, break it and add the hyphen
-//             newText += currentLine + "\n-";
-//             currentLine = words[i];
-//         } else {
-//             currentLine += words[i] + " ";
-//         }
-//     }
-//     if (currentLine.length > 0) {
-//         newText += currentLine;
-//     }
-//     return newText;
-// }
+const pt = {
+    "cmd": "./Apps/CMD/cmd.html",
+    "setting": "./Apps/Setting/setting.html"
+};
 
+let appLists = document.querySelector(".iconsList").getElementsByTagName("ul")[0].querySelectorAll("li")
 
-// document.querySelectorAll(".iconNames").forEach((elem, ind) => {
-//     var textDiv = elem;
-//     var text = textDiv.textContent;
-//     var newText = breakText(text, 40);
-//     // console.log(newText)
-//     textDiv.textContent = newText;
-// })
-
-function importHtml(htmlFile) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", htmlFile, true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var html = xhr.responseText;
-            var element = document.createElement("div");
-            element.innerHTML = html;
-            document.body.appendChild(element);
+for (let apps of appLists) {
+    apps.addEventListener("click", async (e) => {
+        let name = apps.getElementsByTagName("h1")[0].textContent.toLowerCase();
+        if (!document.querySelector(`.${name}`)) {
+            await openApp(name);
+            document.querySelector(`.${name}`).classList.add("minimize");
         }
-    };
-    xhr.send();
+    });
 }
 
-importHtml("./Apps/Terminal/terminal.html")
+const openApp = async (n) => {
+    let p = pt[n]
+    const f = await fetch(p)
+    if (f.ok) {
+        let h = await f.text()
+        let d = document.createElement("div")
+        d.classList.add(n.toLowerCase())
+        d.innerHTML = h
+        document.body.appendChild(d)
+    }
+}
 
-let list = ["terminal"]
+const closeApp = (e) => {
+    let el = e.parentElement.parentElement.parentElement.querySelector("h3").textContent.toLowerCase()
+    let h = document.querySelector(`.${el}`)
+    document.body.removeChild(h)
+}
 
-const appDragger = ({ movementX, movementY }, wrapper) => {
-    let wrapper = document.querySelector(".terminal")
+const resize = (e) => {
+    if (e.parentElement.parentElement.parentElement.classList.contains("maximize")) {
+        minimize(e)
+    } else {
+        maximize(e)
+    }
+}
+
+const maximize = (e) => {
+    if (e.parentElement.parentElement.parentElement.classList.contains("minimize")) {
+        e.parentElement.parentElement.parentElement.classList.remove("minimize")
+    }
+    if (e.parentElement.parentElement.parentElement.classList.contains("maximize")) {
+        return
+    }
+    e.parentElement.parentElement.parentElement.classList.add("maximize")
+    e.parentElement.parentElement.parentElement.style.left = "50%"
+    e.parentElement.parentElement.parentElement.style.top = "50%"
+    e.parentElement.parentElement.parentElement.style.transform = "translate(-50%, -50%)"
+}
+
+const minimize = (e) => {
+    if (e.parentElement.parentElement.parentElement.classList.contains("maximize")) {
+        e.parentElement.parentElement.parentElement.classList.remove("maximize")
+    }
+    if (e.parentElement.parentElement.parentElement.classList.contains("minimize")) {
+        return
+    }
+    e.parentElement.parentElement.parentElement.classList.add("minimize")
+    e.parentElement.parentElement.parentElement.style.left = "50%"
+    e.parentElement.parentElement.parentElement.style.top = "50%"
+    e.parentElement.parentElement.parentElement.style.transform = "translate(-50%, -50%)"
+}
+
+const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            let classes = ["cmd", "setting"]
+            for (let elem of classes) {
+                let e = document.querySelector(`.${elem}`)
+                if (e) {
+                    let n = e.querySelector("nav")
+                    const mouseMoveHandler = function (e) { appDragger(e, elem); }.bind(e);
+                    n.onmouseenter = () => {
+                        n.onmousedown = () => {
+                            if (!document.querySelector(`.${elem}`).querySelector("nav").classList.contains("active")) {
+                                document.querySelector(`.${elem}`).querySelector("nav").classList.add("active")
+                            }
+                            n.addEventListener("mousemove", mouseMoveHandler)
+                        }
+                    }
+                    n.onmouseup = () => {
+                        if (document.querySelector(`.${elem}`).querySelector("nav").classList.contains("active")) {
+                            document.querySelector(`.${elem}`).querySelector("nav").classList.remove("active")
+                        }
+                        n.removeEventListener("mousemove", mouseMoveHandler)
+                    }
+                    n.onmouseleave = () => {
+                        n.removeEventListener("mouseenter", mouseMoveHandler)
+                        n.removeEventListener("mousemove", mouseMoveHandler)
+                        n.removeEventListener("mouseup", mouseMoveHandler)
+                        n.removeEventListener("mouseleave", mouseMoveHandler)
+                    }
+                }
+            }
+        }
+    }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+const appDragger = ({ movementX, movementY }, elem) => {
+    let wrapper = document.querySelector(`.${elem}`)
     let style = window.getComputedStyle(wrapper)
     let leftVal = parseInt(style.left)
     let topVal = parseInt(style.top)
     wrapper.style.left = `${leftVal + movementX}px`
     wrapper.style.top = `${topVal + movementY}px`
 }
-
-let wrap = document.querySelector(".terminal")
-let actionBar = wrap.getElementsByTagName("nav")[0]
-
-actionBar.addEventListener("mousedown", () => {
-    actionBar.addEventListener("mousemove", appDragger)
-})
-actionBar.addEventListener("mouseup", () => {
-    actionBar.removeEventListener("mousemove", appDragger)
-})
