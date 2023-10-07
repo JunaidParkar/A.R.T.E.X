e73aab71cbdcdceafed933a0d9c3186f483b1672
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2023-10-08 01:35:32 +0530

    Fixed bugs

--- Evo/Body/Comp/Js/setting.js
+++ Evo/Body/Comp/Js/setting.js
@@ -1,1 +1,30 @@
-let ttcb = document.querySelector("transparentCheckbx")
+// let ttcb = document.querySelector(".transparentCheckbox").addEventListener('')
+
+// ttcb.addEventListener('click', () => {
+//     console.log("hy")
+// })
+
+const appTranperacyToggle = () => {
+    let ttcb = document.querySelector(".transparentCheckbox").classList
+    if (ttcb.contains("active")) {
+        ttcb.remove("active")
+        let bc = document.body.classList
+        if (bc.contains("noTransparent")) {
+            return
+        }
+        if (bc.contains("transparent")) {
+            bc.remove("transparent")
+        }
+        bc.add("noTransparent")
+    } else {
+        ttcb.add("active")
+        let bc = document.body.classList
+        if (bc.contains("noTransparent")) {
+            bc.remove("noTransparent")
+        }
+        if (bc.contains("transparent")) {
+            return
+        }
+        bc.add("transparent")
+    }
+}
