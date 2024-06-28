f236b1ef46f70e46a62b7017d8bb369bec3839f8
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2024-06-29 01:21:27 +0530

    model working

--- model_loader.py
+++ model_loader.py
@@ -1,27 +1,65 @@
 import pickle
-from typing import Union
+import random
+from nltk.stem.porter import PorterStemmer
 
-class ModelLoader():
+temp_model = "temp_model.txt"
+
+class Model():
     
-    def __init__(self, pth: Union[bool, str] = False) -> None:
+    def __init__(self):
         self.db1 = "intent_quotions.bin"
         self.db2 = "intent_answers.bin"
-        self.pth = pth
-        self.data_to_return = []
+        self.quotions = []
+        self.response = []
+        self.stemmer = PorterStemmer()
 
-    def readData(self):
-        if not self.pth:
-            with open(self.db1, "rb") as db1:
-                self.data_to_return.append(pickle.loads(db1.read()))
-                db1.close()
-            with open(self.db2, "rb") as db2:
-                self.data_to_return.append(pickle.loads(db2.read()))
-                db2.close()
-        else:
-            with open(self.pth, "rb") as pth:
-                self.data_to_return.append(pickle.loads(pth.read()))
-                pth.close()
-        return self.data_to_return
+    def tokenize(self, sentence: str):
+        return sentence.split(" ")
+    
+    def stem(self, sentence: str):
+        return self.stemmer.stem(sentence.lower())
+    
+    def filter_words(self, words: list[any]):
+        stop_words = [",", "/", "?", "!", ":", "'", '"', ".", "...", ".."]
+        return [word for word in words if word.lower() not in stop_words]
 
-a = ModelLoader()
-print(a.readData())
+    def loadData(self):
+        with open(self.db1, "rb") as db1:
+            self.quotions.append(pickle.loads(db1.read()))
+            db1.close()
+        with open(self.db2, "rb") as db2:
+            self.response.append(pickle.loads(db2.read()))
+            db2.close()
+
+    def wordPercentageCalculator(self, query_tokenised: str):
+        set1 = set(query_tokenised)
+        percentile_list = []
+        for data in self.quotions[0]:
+            set2 = set(data[1])
+            intersection = set1.intersection(set2)
+            percentile_list.append([(len(intersection) / len(set1)) * 100, data[0]])
+        if all(num == 0 for num in percentile_list):
+            return [-1]
+        return percentile_list
+
+    def getResponse(self, query: str):
+        query = [self.stem(wrd) for wrd in self.tokenize(query)]
+        print(query)
+        filtered_query = self.filter_words(query)
+        percentile = self.wordPercentageCalculator(filtered_query)
+
+        if percentile[0] == -1:
+            return -1
+        max_percentage = max(percentile, key=lambda x: x[0])
+        print(max_percentage)
+        if max_percentage[0] >= float(75):
+            tag = max_percentage[1]
+            print(tag)
+            response = random.choice(self.response[0][tag])
+            print(response)
+        else: print(None)
+
+a = Model()
+a.loadData()
+# a.getResponse("what is the update on weather tomorrow")
+a.getResponse("go to sleep")
--- trainer.py
+++ trainer.py
@@ -1,11 +1,23 @@
 import json
 import pickle
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
 
 
 class Trainer():
     def __init__(self, path: str) -> None:
         self.file_path = path
-        pass
+        self.stemmer = PorterStemmer()
+
+    def tokenize(self, sentence: str):
+        return word_tokenize(sentence)
+    
+    def stem(self, sentence: str):
+        return self.stemmer.stem(sentence.lower())
+    
+    def filter_words(self, words: list[any]):
+        stop_words = [",", "/", "?", "!", ":", "'", '"', "."]
+        return [word for word in words if word.lower() not in stop_words]
 
     def train(self):
         with open(self.file_path, 'r') as file:
@@ -14,7 +26,11 @@ class Trainer():
             db2 = {}
             for intent in intents:
                 for pattern in intent["patterns"]:
-                    db1.append([intent["tag"], pattern])
+                    ps = pattern.split()
+                    tw = []
+                    for word in ps:
+                        tw.append(self.stem(word.lower()))
+                    db1.append([intent["tag"], self.filter_words(self.tokenize(" ".join(tw)))])
                 db2[intent["tag"]] = intent["responses"]
             
             serialised_data1 = pickle.dumps(db1)
@@ -27,5 +43,6 @@ class Trainer():
                 qb.close()
             file.close()
 
-a = Trainer('intents.json')
+a = Trainer("intents.json")
+# print(a.stem("going"))
 a.train()
